721d31c79cd7c139fcb1c863049b1fcc972dec67
axilirate <56825186+axilirate@users.noreply.github.com>
2021-07-29 20:01:34 +0300

    initial

---
+++ src/main/java/me/axilirate/friendsystem/FriendSystem.java
@@ -0,0 +1,17 @@
+package me.axilirate.friendsystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FriendSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
