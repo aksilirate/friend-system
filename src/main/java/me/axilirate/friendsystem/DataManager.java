24df8caa042ee33b7fce0eee657658d2317e5b30
axilirate <56825186+axilirate@users.noreply.github.com>
2021-08-03 20:53:31 +0300

    Update DataManager.java

--- src/main/java/me/axilirate/friendsystem/DataManager.java
+++ src/main/java/me/axilirate/friendsystem/DataManager.java
@@ -4,6 +4,8 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Set;
 
 public class DataManager {
 
@@ -11,6 +13,36 @@ public class DataManager {
 
     public DataManager(FriendSystem friendSystem) {
         this.friendSystem = friendSystem;
+    }
+
+
+
+    public void setYamlFriendTimeStamp(String playerUID, String friendUID, Double TimeStamp) {
+        File file = new File(friendSystem.getDataFolder(), "/player-data/" + playerUID + ".yml");
+        YamlConfiguration yaml_file = YamlConfiguration.loadConfiguration(file);
+
+        yaml_file.set("friends." + friendUID, TimeStamp);
+
+        saveYamlFile(file, yaml_file);
+    }
+
+
+
+    public Set<String> getYamlFriends(String playerUID){
+        File file = new File(friendSystem.getDataFolder(), "/player-data/" + playerUID + ".yml");
+        YamlConfiguration yaml_file = YamlConfiguration.loadConfiguration(file);
+
+        return yaml_file.getKeys(false);
+    }
+
+
+
+    public Double getYamlFriendTimeStamp(String playerUID, String friendUID) {
+        File file = new File(friendSystem.getDataFolder(), "/player-data/" + playerUID + ".yml");
+        YamlConfiguration yaml_file = YamlConfiguration.loadConfiguration(file);
+
+        return (Double) yaml_file.get("friends." + friendUID);
+
     }
 
 
